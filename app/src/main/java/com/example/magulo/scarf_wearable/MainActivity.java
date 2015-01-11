4df2fd2821feb08c2473b3e8d6fac92bb1c93f75
nnaik39 <nanni.naik@gmail.com>
2015-01-10 16:44:49 -0800

    added new activity

--- app/src/main/java/com/example/magulo/scarf_wearable/MainActivity.java
+++ app/src/main/java/com/example/magulo/scarf_wearable/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.magulo.scarf_wearable;
 
-bimport android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,12 +12,20 @@ import android.widget.ImageView;
 public class MainActivity extends ActionBarActivity {
     private ImageView imageView;
     private Button button;
+    private Button button_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
         imageView = (ImageView) findViewById(R.id.imageView);
+        button_1 = (Button) findViewById(R.id.button_1);
+        button_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TempSetter.class);
+                startActivity(intent);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (imageView.isShown() == true)
