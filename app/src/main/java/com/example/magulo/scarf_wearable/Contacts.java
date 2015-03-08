b151cf8415cbd313ebf1e87dc7c92a73d2ce4752
nnaik39 <nanni.naik@gmail.com>
2015-03-08 11:54:34 -0700

    added calling options

--- app/src/main/java/com/example/magulo/scarf_wearable/Contacts.java
+++ app/src/main/java/com/example/magulo/scarf_wearable/Contacts.java
@@ -2,22 +2,35 @@ package com.example.magulo.scarf_wearable;
 
 import android.Manifest;
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.view.View;
 import android.widget.AdapterView;
 import android.provider.ContactsContract;
+import android.widget.Button;
 
 public class Contacts extends ActionBarActivity {
-
+    private Button button;
+    String phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                phoneNumber = "6505551234";
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));//area code and number, no spaces
+                startActivity(intent);
+            }
+        });
     }
 
 
